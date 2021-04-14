f519c4c55b88799ca1b8457b40ffdc346145acc5
Ciantera-Rose <ciantera.rose@gmail.com>
2021-04-14 16:17:26 -0400

    add classes

---
+++ OOP.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+    OOP: 
+    - In OOP it’s useful to want our types to not only hold data, but provide functions that work with the data as well.
+    - Functions defined inside of a class are called member functions or methods.
+    - Methods can be defined inside or outside of the class definition
+    - The class keyword defines a new user-defined type called a class.
+    - The public keyword is also used before declaring the member variables.
+    - An Object is an instance of a Class. When a class is defined, no memory is allocated but when it is instantiated, 
+      like when an object is created, memory is allocated. So a class is a user-defined data type that we can use in our 
+      program, and it works as an object constructor, or a “blueprint” for creating objects.
+
+*/
+
+class DateClass 
+{
+    public:
+    int m_year;
+    int m_month;
+    int m_day;
+
+    void print()
+    {
+         cout << "The date today is " << m_month << "/" << m_day << "/" << m_year << endl;
+    }
+};
+
+int main () 
+{
+    DateClass today = {2021, 04, 14};
+
+    today.print();
+    return 0;
+}
+
+
