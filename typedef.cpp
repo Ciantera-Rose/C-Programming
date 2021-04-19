cb8b8cd9aec40b650fa761f6463cc97cc0293181
Ciantera-Rose <ciantera.rose@gmail.com>
2021-04-19 13:05:36 -0400

    add brief intro to typedef

---
+++ typedef.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* 
+    Typedef: Brief intro
+    - Creates an alias that can be used anywhere in place of a (possibly complex) type name.
+    - Helps when using templates and to manage complex/massive data types.
+    - The typedef specifier, when used in a declaration, specifies that the declaration is a typedef 
+      declaration rather than a variable or function declaration.
+    - 
+ */
+
+struct Homosapiens {
+    string name;
+};
+
+typedef Homosapiens Person;
+
+typedef double height;
+
+int main() {
+
+    height cianHeight = 191.5;
+    Person person;
+    person.name;
+
+    cout << cianHeight << endl;
+    return 0;
+}
